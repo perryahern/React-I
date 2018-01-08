ef2ab5264c621b3b4b4707c3080a243c188bace1
Perry <pahern@gmail.com>
2018-01-08 15:56:16 -0500

    Test push, replaced CS4 files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-/*eslint no-unused-vars: "off"*/
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-
-// You'll want to import the necessary components you want the App component to render
-import App from './components/App';
+import App from './App';
 
 render(<App />, document.getElementById('root'));
